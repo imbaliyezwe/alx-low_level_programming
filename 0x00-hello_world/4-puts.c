27040400c840219e8cb9454dc6a9f9466725ff72
imbaliyezwe <nomkithawilliams@gmail.com>
2023-02-17 00:37:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - main block
+ * main - print a line of code using puts
  * Return : 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
